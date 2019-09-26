963279af8ad52d8b6b37fd93aba8745d635f57c8
Erik Didriksen <erikdidriksen@gmail.com>
2019-09-26 18:17:09 -0400

    Fix docstring

--- spin2spot/spotify.py
+++ spin2spot/spotify.py
@@ -6,7 +6,7 @@ from .retrieval import retrieve_episode
 
 
 def get_username(username=None):
-    """Retrieves the usrename from environment variables if none specified."""
+    """Retrieves the username from environment variables if none specified."""
     username = username or os.environ.get('SPIN2SPOT_USERNAME')
     if username:
         return username
