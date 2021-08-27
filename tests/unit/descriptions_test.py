0d2de2d58cbfa4232c9f3af03bb04c0d0c608f2c
Erik Didriksen <erikdidriksen@gmail.com>
2021-08-27 10:47:20 -0400

    Break description test dependencies on fixtures

--- tests/unit/descriptions_test.py
+++ tests/unit/descriptions_test.py
@@ -1,29 +1,27 @@
+import datetime
 import pytest
-import fixtures
-from spin2spot import descriptions, parsers
+from spin2spot import descriptions
 
 
-setlist_fm = parsers.SetlistFMParser(fixtures.soup('setlist_fm.html'))
-spinitron = parsers.SpinitronParser(fixtures.soup('spinitron_v2.html'))
-wkdu = parsers.WKDUParser(fixtures.soup('wkdu.html'))
-wprb = parsers.WPRBParser(fixtures.soup('wprb.html'))
+title = 'Spin Cycle'
+dt = datetime.datetime(2021, 8, 27, 9)
+date = datetime.datetime(2021, 8, 27)
+station = 'WERA'
+dj = 'Lauree'
+venue = 'TV Eye'
 
 
 @pytest.mark.parametrize('parser, expected', [
-    (setlist_fm, 'At Brooklyn Bowl, Brooklyn, NY, USA'),
-    (spinitron, 'Tuesday at 2:00pm on WZBC 90.3 FM Newton with Nick'),
-    (wkdu, 'Tuesday on WKDU with Matt'),
-    (wprb, 'Tuesday at 2:00pm on WPRB with Bad Newz'),
+    ({'venue': venue}, 'At TV Eye'),
+    ({'datetime': dt, 'station': station, 'dj': dj}, 'Friday at 9:00am on WERA with Lauree'),
+    ({'datetime': date, 'station': station, 'dj': dj}, 'Friday on WERA with Lauree'),
     ])
 def test_builds_playlist_description_from_parser(parser, expected):
     assert descriptions.playlist_description(parser) == expected
 
 
 @pytest.mark.parametrize('parser, expected', [
-    (setlist_fm, 'The Lemonheads: May 04, 2019'),
-    (spinitron, '7DayWknd: August 02, 2016'),
-    (wkdu, 'The New Matt Show: October 01, 2013'),
-    (wprb, 'Ruckus Radio: November 10, 2015'),
+    ({'title': title, 'datetime': dt}, 'Spin Cycle: August 27, 2021'),
     ])
 def test_builds_playlist_title_from_parser(parser, expected):
-    return descriptions.playlist_title(parser) == expected
+    assert descriptions.playlist_title(parser) == expected
